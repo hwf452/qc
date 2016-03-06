3e95fdd42cec14832641152b18062b422793330d
Jakub Petrik <petrik@inloop.eu>
2016-03-06 16:22:51 +0100

    Refactor

---
+++ qc/QC.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+struct QC {
+  
+  static func run(arguments: [String]) {
+    NSUserDefaults.initialize()
+    
+    if arguments.hasOption(Options.setPassword) {
+      setPassword(arguments.argumentForOption(Options.setPassword))
+    }
+    else if arguments.hasOption(Options.printUsage) {
+      printUsage()
+    }
+    else {
+      runScript()
+    }
+    
+    exit(EXIT_SUCCESS)
+  }
+  
+  static func setPassword(newPassword: String?) {
+    guard let password = newPassword else {
+      printUsage()
+      exit(EXIT_FAILURE)
+    }
+    
+    NSUserDefaults.password = password
+  }
+  
+  static func printUsage() {
+    print("usage: qc                             connect to VPN")
+    print("   or: qc \(Options.setPassword) <password>        set password")
+    print("   or: qc \(Options.printUsage)                      print help")
+    print("\nArguments:")
+    print("  \(Options.setPassword)\t\tSet a new password to be used")
+    print("  \(Options.printUsage)\t\tPrint Help (this message) and exit")
+  }
+  
+  static func runScript() {
+    guard let password = NSUserDefaults.password else {
+      print("Password not found!")
+      printUsage()
+      exit(EXIT_FAILURE)
+    }
+    
+    let format =
+    "activate application \"Cisco AnyConnect Secure Mobility Client\"\n" +
+      "tell application \"System Events\" to tell process \"Cisco AnyConnect Secure Mobility Client\"\n" +
+      "click button \"Connect\" of window 1\n" +
+      "repeat until window 2 exists\n" +
+      "end repeat\n" +
+      "keystroke \"%@\"\n" +
+      "click button \"OK\" of window 1\n" +
+    "end tell\n"
+    
+    let source = String(format: format, password)
+    guard let script = NSAppleScript(source: source) else {
+      print("Could not create script!")
+      printUsage()
+      exit(EXIT_FAILURE)
+    }
+    
+    script.executeAndReturnError(nil)
+  }
+  
+}
